d45b150fcb7c26b75a4f642d414a7bc22f392a35
jeuunKim <ygvhn753@naver.com>
2023-12-13 16:15:35 +0900

    로그추적기, 쓰레드로컬 연습

---
+++ advanced/src/main/java/hello/advanced/app/v0/OrderControllerV0.java
@@ -0,0 +1,23 @@
+package hello.advanced.app.v0;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV0 {
+
+    private final OrderServiceV0 orderService;
+
+    @GetMapping("/v0/request")
+    public String request(@RequestParam("itemId") String itemId){
+
+        orderService.orderItem(itemId);
+
+        return "ok";
+    }
+}
