a2b8d03304e8ee3c61a6a8c50651e0cbd4424b22
jeuunKim <ygvhn753@naver.com>
2023-12-14 16:30:46 +0900

    로그추적기, 쓰레드로컬, 템플릿메소드, 전략패턴 연습

---
+++ advanced/src/test/java/hello/advanced/trace/strategy/code/strategy/ContextV2.java
@@ -0,0 +1,22 @@
+package hello.advanced.trace.strategy.code.strategy;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ContextV2 {
+    /**
+     * 전략을 파라미터로 전달 받는 방식
+     */
+
+    public void execute(Strategy strategy){
+        long startTime = System.currentTimeMillis();
+
+        strategy.call();
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+
+
+}
